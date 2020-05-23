8176ad32cbcd2fef607c999cfd56525504109251
nicholagon <nicholagon@gmail.com>
2020-05-24 01:50:36 +0800

    Problem Set 1 - C - Mario Less and Mario More

---
+++ pset1/mario_less/mario.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int height;
+
+    do
+    {
+        height = get_int("Height (1-8): ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = height; j > 0; j--)
+        {
+            if (j <= i)
+            {
+                printf("#");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
